e4191ef46692010d4b8db1781c2b0ea4a33ac5bf
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-12-15 18:43:18 +0900

    feat(GiftEventPolicy) : 증정 이벤트 기능 구현

---
+++ src/main/java/christmas/domain/eventpolicy/GiftEventPolicy.java
@@ -0,0 +1,25 @@
+package christmas.domain.eventpolicy;
+
+import static christmas.consts.MenuBoard.getPriceByMenu;
+
+import christmas.domain.customer.Customer;
+import christmas.domain.menu.Menu;
+
+public class GiftEventPolicy implements EventPolicy {
+
+    private static final int APPLICABLE_TOTAL_AMOUNT = 12_000;
+    private static final Menu GIFT_MENU = new Menu("샴페인");
+
+    @Override
+    public boolean applicableEvent(Customer customer) {
+        return customer.applicableEvent() && customer.getTotalOrderAmount() >= APPLICABLE_TOTAL_AMOUNT;
+    }
+
+    @Override
+    public int getBenefitAmount(Customer customer) {
+        if (applicableEvent(customer)) {
+            return getPriceByMenu(GIFT_MENU);
+        }
+        return 0;
+    }
+}
