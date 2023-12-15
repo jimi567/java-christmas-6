5b0f90f2d686474616c90264ff53373ffbcc87fc
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-12-15 18:50:21 +0900

    feat(EventBadge) : 이벤트 배지 Enum 구현
    각 상수와 혜택 금액에 따른 이벤트 배지를 반환하는 메서드 구현

---
+++ src/main/java/christmas/consts/EventBadge.java
@@ -0,0 +1,36 @@
+package christmas.consts;
+
+public enum EventBadge {
+    NONE("없음", 0),
+    STAR("별", 5_000),
+    TREE("트리", 10_000),
+    SANTA("산타", 20_000);
+
+
+    private final String name;
+    private final int minimumBenefitAmount;
+
+    EventBadge(String name, int minimumBenefitAmount) {
+        this.name = name;
+        this.minimumBenefitAmount = minimumBenefitAmount;
+    }
+
+    public int getMinimumBenefitAmount() {
+        return minimumBenefitAmount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static EventBadge getEventBadgeByBenefitAmount(int benefitAmount) {
+        if (SANTA.getMinimumBenefitAmount() >= benefitAmount) {
+            return SANTA;
+        } else if (TREE.getMinimumBenefitAmount() >= benefitAmount) {
+            return TREE;
+        } else if (STAR.getMinimumBenefitAmount() >= benefitAmount) {
+            return STAR;
+        }
+        return NONE;
+    }
+}
