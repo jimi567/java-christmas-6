b4e77605bfdb5a369b786e7db8b1c1e426a13c39
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-12-15 18:33:22 +0900

    feat(DdayEventPolicy) : 디데이 이벤트 정책 구현

---
+++ src/main/java/christmas/domain/eventpolicy/DdayEventPolicy.java
@@ -0,0 +1,25 @@
+package christmas.domain.eventpolicy;
+
+import static christmas.consts.DecemberCalender.CHRISTMAS;
+
+import christmas.domain.customer.Customer;
+
+public class DdayEventPolicy implements EventPolicy {
+
+    private static final int START_BENEFIT_AMOUNT = 1000;
+    private static final int PLUS_BENEFIT_AMOUNT = 100;
+
+    @Override
+    public boolean applicableEvent(Customer customer) {
+        return customer.applicableEvent() && customer.visitDate().isBeforeDday();
+    }
+
+    @Override
+    public int getBenefitAmount(Customer customer) {
+        if (applicableEvent(customer)) {
+            return START_BENEFIT_AMOUNT + (PLUS_BENEFIT_AMOUNT * (CHRISTMAS - customer.visitDate()
+                    .calculateDaysUntilDday() - 1));
+        }
+        return 0;
+    }
+}
