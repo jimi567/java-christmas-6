a7fe2367f5a9d19849dfe717936d98c764a5eada
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-12-15 16:37:23 +0900

    feat(DecemberCalender) : 달력 상수 Enum 클래스 추가

---
+++ src/main/java/christmas/consts/DecemberCalender.java
@@ -0,0 +1,26 @@
+package christmas.consts;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public enum DecemberCalender {
+    EVENT_DAYS(IntStream.rangeClosed(1, 31)
+            .boxed()
+            .toList()),
+    D_DAY_EVENT_DAYS(IntStream.rangeClosed(1, 25)
+            .boxed()
+            .toList()),
+    STAR_DAYS(List.of(3, 10, 17, 24, 31, 25)),
+    WEEKEND_DAYS(List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30));
+
+
+    private final List<Integer> days;
+
+    DecemberCalender(List<Integer> days) {
+        this.days = days;
+    }
+
+    public List<Integer> get() {
+        return days;
+    }
+}
