e3393eb3131da4fd85cbaf61135b88d0d3563c55
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-12-15 18:38:43 +0900

    feat(WeekendDayEventPolicy) : 주말 이벤트 정책 기능 구현

---
+++ src/main/java/christmas/domain/eventpolicy/WeekendDayEventPolicy.java
@@ -0,0 +1,23 @@
+package christmas.domain.eventpolicy;
+
+import static christmas.consts.MenuBoard.MAIN;
+
+import christmas.domain.customer.Customer;
+
+public class WeekendDayEventPolicy implements EventPolicy {
+
+    private static final int BENEFIT_AMOUNT = 2023;
+
+    @Override
+    public boolean applicableEvent(Customer customer) {
+        return customer.applicableEvent() && customer.visitDate().isWeekendDay();
+    }
+
+    @Override
+    public int getBenefitAmount(Customer customer) {
+        if (applicableEvent(customer)) {
+            return BENEFIT_AMOUNT * customer.orderMenu().countMenusByCategory(MAIN);
+        }
+        return 0;
+    }
+}
